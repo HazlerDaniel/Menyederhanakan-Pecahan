d2e30ceb978fe7627840fdddda42c968c1774e5d
Daniel Xiha <40915588+HazlerDaniel@users.noreply.github.com>
2018-12-30 18:42:11 +0700

    Add files via upload

---
+++ Menyederhanakan Pecahan.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<iostream>
+
+void pecahan();
+int main(){
+	pecahan();
+}
+	void pecahan(){
+		int a,b,l,t;
+		printf("Masukan pembilang :");
+		scanf("%d",&a);
+		printf("Masukan Penyebut :");
+		scanf("%d",&b);
+		t=a/b;
+		l=a%b;
+		if(a%b==0)
+		printf("Bilangan (%d/%d)\n di sederhanakan menjadi %d",a,b,t);
+		else if(a%b!=0)
+		printf("Bilangan (%d/%d)\n disederhanakan menjadi (%d %d/%d)",a,b,t,l,b);
+		
+		getchar();
+	}
